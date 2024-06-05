403a0991c39db495acc3c2e3ceaee34d4986c4e1
luislunav <luis.a.luna.v@outlook.com>
2024-06-05 14:18:35 -0600

    Terminamos el controlador de conteo de votos

---
+++ src/services/api-votos.js
@@ -0,0 +1,47 @@
+const url = "http://localhost:8080";
+const apiVotos = '/api/v1/votos/';
+const postVotos = async (data) => {
+    try {
+      const resp = await fetch(`${url}${apiVotos}${'agregar'}`, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
+  
+      if (resp.ok) {
+        const data = await resp.json();
+        return data;
+      } else{
+        const error = await resp.json();
+        throw error;
+      } 
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const getVotos = async(  )=>{
+    try {
+        const resp = await fetch(`${ url }${ apiVotos }${contar}`);
+       
+        if ( resp.ok ) {
+            const data = await resp.json();
+
+            return data;
+ 
+        } else{
+            const { errors } = await resp.json();
+            throw errors;
+        }
+        
+    } catch (error) {
+        throw error;
+    }
+};
+
+  export{
+    postVotos,
+    getVotos
+  }
